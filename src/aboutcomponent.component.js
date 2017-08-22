adb35d164dfb9084470a4fcdcf71558c4036022f
Enrico Tuvera Jr <enrico@elephantventures.com>
2017-08-22 20:12:11 +0800

    updated some stuff; cant remember what

--- src/aboutcomponent.component.js
+++ src/aboutcomponent.component.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import {
     Row, 
-    Col
+    Col,
+    Image,
+    Panel
 } from 'react-bootstrap';
+import Watchdogman from './Watchdogman2.png';
 
 export default class AboutComponent extends React.Component {
   render() {
     return (
       <Row>
-          <Col md={6}>
-            <h1> Left </h1>
+          <Col md={2}></Col>
+          <Col md={4}>
+            <Image src={Watchdogman}></Image>
             </Col>
           <Col md={6}>
-            <h1> Right </h1>
-            </Col>
+            <Panel header="About">
+              The author was an S-Class Rank 12 professional hero who was in charge of Q-City. 
+              <hr></hr> 
+              Following a long,
+              illustrious career as a superhero, the author retired to pursue a degree in Computer Science and now
+              works as a software engineering, tackling engineering problems with the same ferocity and skill he had when 
+              he ran around tearing monsters to pieces while dressed like a dog.
+            </Panel>
+          </Col>
       </Row>
     )
   }
--- src/todolist.component.js
+++ src/todolist.component.js
@@ -10,7 +10,7 @@ export default class TodoListComponent extends React.Component {
     let reg_list = this.props.todos.map( (elem, index) => {
       return (
         <ListGroupItem key={index}>{elem}<span className="btn btn-xs btn-default pull-right" onClick={this.delete.bind(this, index)}>
-        <Glyphicon glyph="remove" /></span>
+        <Glyphicon glyph="remove"/></span>
         </ListGroupItem>
       )
     })
